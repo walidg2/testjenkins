dc2a2379c4fb5dcadc5baa60a35815ea40546e58
walidg2 <walid.gafsi@gmail.com>
2018-06-15 23:55:26 +0200

    init

---
+++ src/main/java/org/walid/App.java
@@ -0,0 +1,13 @@
+package org.walid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
